62c653ba46c9bfc50ad40e770f82be3e02f3e2a7
Naysuru <julien.frey@etu.hesge.ch>
2023-04-27 23:00:21 +0200

    Petit ajout de commentaire en despi c'est du gpt

--- codes_python/MQTT_mosquitto_Server.py
+++ codes_python/MQTT_mosquitto_Server.py
@@ -1,24 +1,35 @@
-import paho.mqtt.client as mqtt #import the client1
+# Import de la bibliothèque Paho MQTT, time et keyboard
+import paho.mqtt.client as mqtt
 import time
 import keyboard
 
+# Définition d'une fonction callback qui sera appelée chaque fois qu'un message est reçu sur le topic abonné
 def on_message(client, userdata, message):
     print("message received " ,str(message.payload.decode("utf-8")))
     print("message topic=",message.topic)
     print("message qos=",message.qos)
     print("message retain flag=",message.retain)
 
+# Définition de l'adresse IP du broker MQTT
 broker_address="192.168.X.Y"
+
+# Création d'une nouvelle instance du client MQTT
 print("creation de la nouvelle instance")
 client = mqtt.Client("client1") 
+
+# Association de la fonction callback "on_message" au client MQTT
 client.on_message=on_message 
+
+# Connexion du client MQTT au broker MQTT et démarrage de la boucle de réception des messages
 print("connection au broker")
 client.connect(broker_address) 
-client.loop_start() #demarrage de la boucle
+client.loop_start()
+
+# Abonnement du client MQTT au topic "salon/temp"
 print("Inscription au topic","salon/temp")
 client.subscribe("salon/temp")
-#print("Publishing message to topic","salon/temp")
-#client.publish("salon/temp","23.4")
+
+# Boucle while pour laquelle l'utilisateur doit appuyer sur la touche "q" pour terminer le programme
 fin = False
 while fin == False:  
     if keyboard.is_pressed('q'):  
@@ -27,4 +38,5 @@ while fin == False:
     #ToDo----------
     #FinToDo----------
 
+# Arrêt de la boucle de réception des messages
 client.loop_stop()
